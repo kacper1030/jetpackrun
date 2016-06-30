a5bd718d3db99da8deef6bfebfae36f9b0bc1b57
Kacper Jakobczak <kacper.jakobczak15@onet.pl>
2016-06-30 18:33:52 +0200

    Upload all input

--- core/src/com/kacperj/jetpackrun/screens/GameplayScreen.java
+++ core/src/com/kacperj/jetpackrun/screens/GameplayScreen.java
@@ -7,66 +7,73 @@ import com.kacperj.jetpackrun.JetpackRun;
 import com.kacperj.jetpackrun.gameobject.GameObject;
 import com.kacperj.jetpackrun.gameobject.JumpPlayer;
 
-public class GameplayScreen extends AbstractScreen{
+public class GameplayScreen extends AbstractScreen {
 
 	private JumpPlayer jumpPlayer;
 	private GameObject playerObj;
 	private Texture playerTex;
-	
-	private float gravity = -13;
-	
+
+	private float gravity = -12;
+
 	public GameplayScreen(JetpackRun game) {
 		super(game);
 		init();
 	}
-	
+
 	private void init() {
-		
+
 		jumpPlayer = new JumpPlayer();
 		playerTex = new Texture("badlogic.jpg");
 		playerObj = new GameObject(playerTex);
-		
+
 		playerObj.x = 0;
 		playerObj.y = 0;
 		playerObj.height = playerObj.getTexture().getHeight();
 		playerObj.width = playerObj.getTexture().getWidth();
 	}
+
 	@Override
 	public void render(float delta) {
 		super.render(delta);
-		
+
 		update();
-		
+
 		batch.begin();
-		
+
 		batch.draw(playerObj.getTexture(), playerObj.x, playerObj.y);
-		
+
 		batch.end();
-		
+
 	}
 
 	private void update() {
 		playerObj.y += jumpPlayer.jumpVelocity * Gdx.graphics.getDeltaTime();
-		
-		if(playerObj.y > 0){
+
+		if (playerObj.y > 0) {
 			jumpPlayer.jumpVelocity += gravity;
 		} else {
 			playerObj.y = 0;
 			jumpPlayer.canJump = true;
 			jumpPlayer.jumpVelocity = 0;
 		}
-		
+
 		movePlayer();
-		
+
 	}
 
 	private void movePlayer() {
-		if(Gdx.input.isKeyPressed(Keys.W)){
+		if (Gdx.input.isKeyPressed(Keys.W)) {
 			playerObj.y += 500 * Gdx.graphics.getDeltaTime();
+		}
+		if (Gdx.input.isKeyPressed(Keys.S)) {
+			playerObj.y -= 100 * Gdx.graphics.getDeltaTime();
+		}
+		if (Gdx.input.isKeyPressed(Keys.A)) {
+			playerObj.x -= 100 * Gdx.graphics.getDeltaTime();
+		}
+		if (Gdx.input.isKeyPressed(Keys.D)) {
+			playerObj.x += 100 * Gdx.graphics.getDeltaTime();
 		}
 	}
 
 }
-
-
-
